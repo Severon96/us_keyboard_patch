2a6480e496288bda4056db7017b867bcca7b06a9
Marco Quinten <splittydev@protonmail.com>
2019-03-18 14:51:04 +0100

    Add root check, refactor

--- us_keyboard_patch.sh
+++ us_keyboard_patch.sh
@@ -1,5 +1,25 @@
-mv us_patch.diff /usr/share/X11/xkb/symbols
-cd /usr/share/X11/xkb/symbols/us_patch.diff
+#!/usr/bin/env bash
+
+# root check
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
+# copy patch to keyboard data folder
+cp us_patch.diff /usr/share/X11/xkb/symbols
+
+# change directory
+pushd /usr/share/X11/xkb/symbols/us_patch.diff
+
+# create a backup of the original us keyboard data
 cp us us.backup
+
+# apply the patch
 git apply us_patch.diff
-sudo dpkg-reconfigure xkb-data
+
+# reconfigure X11 keyboard extension data
+dpkg-reconfigure xkb-data
+
+# restore directory
+popd
